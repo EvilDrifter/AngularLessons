34c962fe14c4abcad64822ad0193236573c606b5
MadMax <klenov.em@gmail.com>
2019-06-08 05:44:12 +0100

    add a modelto TS and data to C#

---
+++ AngularLessons/ClientApp/src/app/models/IDomNode.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=IDomNode.js.map
---
+++ AngularLessons/ClientApp/src/app/models/IDomNode.ts
@@ -0,0 +1,8 @@
+interface IDomNode {
+  tag: string | undefined;
+  attributes: {
+    [key: string]: string
+  };
+  content: IDomNode[];
+  text?: string;
+}
