93e7071ff69faf36d998a1063de5720b8e79e99e
MadMax <klenov.em@gmail.com>
2019-06-08 05:48:23 +0100

    add controller that reads json file on a server side

---
+++ AngularLessons/Controllers/JsonDataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngularLessons.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JsonDataController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        [Route("getJson")]
+        public string GetJsonFile()
+        {
+            return System.IO.File.ReadAllText("./Controllers/data.json");
+        }
+    }
+}
